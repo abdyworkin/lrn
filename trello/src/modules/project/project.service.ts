877c464449a672709b54666dc61710050c0aa262
Abdrakhman Safin <abdyworkin@gmail.com>
2024-06-22 01:49:16 +0300

    project service transaction scalable

--- trello/src/modules/project/project.service.ts
+++ trello/src/modules/project/project.service.ts
@@ -6,7 +6,6 @@ import { DataSource, EntityManager, Repository } from 'typeorm';
 import { UserToProject, ProjectRoles } from '../../entities/user_to_project.entity';
 import { Project } from './project.entity';
 import { FieldType, ProjectTaskFieldEnumOptions, ProjectTaskField } from 'src/modules/field/project_field.entity';
-import { AddFieldDto, EditFieldDto } from '../field/field.dto';
 import { FieldService } from '../field/field.service';
 import { CreateProjectDto, UpdateProjectDto } from './project.dto';
 
@@ -47,18 +46,21 @@ export class ProjectService {
         return await this.dataSource.transaction(async manager => await func(manager))
     }
 
-    async getAllUserProjects(userId: number) {
-        const projectsRelated = await this.userToProjectRepository.find({ 
+    async getAllUserProjects(userId: number, manager?: EntityManager) {
+        const pUserRepo = manager?.getRepository(UserToProject) || this.userToProjectRepository
+
+        const projectsRelated = await pUserRepo.find({ 
             where: { userId }, 
-            relations: projectRelations.map(e => `project.${e}`)
+            relations: projectRelations.map(e => `project.${e}`) //TODO: требуется оптимизация, в отношении не должны догружаться таски
         })
 
         const projects = projectsRelated.map(e => e.project)
         return projects
     }
 
-    async getProject(id: number) {
-        const project = await this.projectRepository.findOne({ where: { id }, relations: projectRelations })
+    async getProject(id: number, manager?: EntityManager) {
+        const projectRepo = manager?.getRepository(Project) || this.projectRepository
+        const project = await projectRepo.findOne({ where: { id }, relations: projectRelations })
         return project
     }
 
@@ -105,26 +107,32 @@ export class ProjectService {
         return await projectRepo.save(project)
     }
 
-    async deleteProject(projectId: number) {
-        const {affected} = await this.projectRepository.delete(projectId)
+    async deleteProject(projectId: number, manager?: EntityManager) {
+        const projectRepo = manager?.getRepository(Project) || this.projectRepository
+        const {affected} = await projectRepo.delete(projectId)
         return affected > 0
     }
 
-    async createInviteCode(project: Project) {
+    async createInviteCode(project: Project, manager?: EntityManager) {
+        const projectRepo = manager?.getRepository(Project) || this.projectRepository
+
         const [inviteCode, expires] = this.generateInviteCode()
         
         project.inviteCode = inviteCode
         project.inviteExpires = expires
 
-        return await this.projectRepository.save(project)
+        return await projectRepo.save(project)
     }
 
     private generateInviteCode(): [string, number] {
         return [hashSync(Math.random().toString(), 10).replaceAll('/', ''), Date.now() + (1000 * 60 *  60 * 24)]
     }
 
-    async joinByCode(code: string, projectId: number, user: User) {
-        const project = await this.projectRepository.findOne({ where: { id: projectId }, relations: [ 'users.user' ] })
+    async joinByCode(code: string, projectId: number, user: User, manager?: EntityManager) {
+        const projectRepo = manager?.getRepository(Project) || this.projectRepository
+        const userToProjectRepo = manager?.getRepository(UserToProject) || this.userToProjectRepository
+
+        const project = await projectRepo.findOne({ where: { id: projectId }, relations: [ 'users.user' ] })
 
         if(!project) throw new NotFoundException('project/not-found')
         if(project.users.findIndex(e => e.userId === user.id) !== -1) return project
@@ -137,35 +145,40 @@ export class ProjectService {
         userToProject.user = user
         userToProject.projectId = project.id
         userToProject.role = ProjectRoles.User
-        await this.userToProjectRepository.save(userToProject)
+        await userToProjectRepo.save(userToProject)
 
         project.users = [...project.users, userToProject]
 
-        return await this.projectRepository.save(project)
+        return await projectRepo.save(project)
     }
 
-    async kickUser(userId: number, project: Project, ban?: boolean) {
+    async kickUser(userId: number, project: Project, ban: boolean, manager?: EntityManager) {
+        const userToProjectRepo = manager?.getRepository(UserToProject) || this.userToProjectRepository
+
         if(ban) {
-            const result = await this.userToProjectRepository.update({ userId, projectId: project.id }, { role: ProjectRoles.Banned })
+            const result = await userToProjectRepo.update({ userId, projectId: project.id }, { role: ProjectRoles.Banned })
             return result.affected === 1
         }
 
         // Просто убираем пользователя
-        const result = await this.userToProjectRepository.delete({ userId: userId, projectId: project.id })
+        const result = await userToProjectRepo.delete({ userId: userId, projectId: project.id })
         return result.affected === 1
     }
 
-    async leaveProject(user: User, project: Project): Promise<boolean> {
+    async leaveProject(user: User, project: Project, manager?: EntityManager): Promise<boolean> {
+        const projectRepo = manager?.getRepository(Project) || this.projectRepository
+        const userToProjectRepo = manager?.getRepository(UserToProject) || this.userToProjectRepository
+
         //Крайний случай: последний пользователь выходит из проекта, проект удаляется
         if (project.users.length === 1 && project.users[0].user.id === user.id) {
-            const result = await this.projectRepository.remove(project)
+            const result = await projectRepo.remove(project)
             return !!result
         }
 
         //TODO: давать роль модератора, либо пользователю, которого назначил бывший модератор, либо следующему по списку
         //TODO: сделать нормальный вывод
         const userToProjectEntity = project.users.filter(e => e.user.id === user.id)[0]
-        const result = await this.userToProjectRepository.remove(userToProjectEntity)
+        const result = await userToProjectRepo.remove(userToProjectEntity)
         return !!result
     }
 }
